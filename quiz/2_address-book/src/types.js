ee2d4134307e43f55bc1aa78908968858ef7a8ba
fancyers <kjng5901@gmail.com>
2022-04-09 18:37:54 +0900

    learn typescript

---
+++ quiz/2_address-book/src/types.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.PhoneType = void 0;
+var PhoneType;
+(function (PhoneType) {
+    PhoneType["Home"] = "home";
+    PhoneType["Office"] = " office";
+    PhoneType["Studio"] = "studio";
+})(PhoneType || (PhoneType = {}));
+exports.PhoneType = PhoneType;
